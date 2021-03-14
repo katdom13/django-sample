ec6226ad54c8a553f1c28f84859389cd2554b837
katdom13 <katdom13@gmail.com>
2021-03-14 21:33:02 +0800

    Enhancement: Delete old profile picture when updated

--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
@@ -13,3 +13,16 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+@receiver(pre_save, sender=Profile)
+def save_image(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_image = Profile.objects.get(pk=instance.pk).image
+        except Profile.DoesNotExist:
+            return
+        else:
+            new_image = instance.image
+            if old_image and old_image.url != new_image.url:
+                old_image.delete(save=False)
